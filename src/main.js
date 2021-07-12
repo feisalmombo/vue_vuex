daa1b15ad330030733dca2ae77472379df4cb520
Feisal Mombo <feisalmombo29@gmail.com>
2021-07-12 16:03:02 +0300

    add package for vue-router and vuex

--- src/main.js
+++ src/main.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import App from './App.vue'
-import VueResource from 'vue-resource';
+import VueResource from 'vue-resource'
+import store from './store'
 
 Vue.use(VueResource);
 
@@ -9,5 +10,6 @@ Vue.http.options.root = "https://jsonplaceholder.typicode.com/";
 Vue.config.productionTip = false
 
 new Vue({
+    store,
     render: h => h(App),
 }).$mount('#app')
