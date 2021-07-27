3672c2335acdfbb48dce8dce94e9dbc13c06f233
Feisal Mombo <feisalmombo29@gmail.com>
2021-07-28 00:20:04 +0300

    modify driver.vue file

--- src/views/Driver.vue
+++ src/views/Driver.vue
@@ -3,7 +3,15 @@
     <p>{{ comment}}</p>
 
     <h2>{{ fname }}</h2>
-    <input type="text" name="fname" v-model="fname" placeholder="First Name">
+    <input v-text="fname" type="text" name="fname" v-model="fname" placeholder="First Name">
+    <br>
+
+    <p>My name is {{name}}</p>
+    <button @click="changeName()">Change my name!</button>
+    <br>
+    <!-- Events -->
+    <!-- <a v-on:click="handleClick">Click me!</a> -->
+    <a @click="handleClick">Click me!</a>
 </div>
 </template>
 
@@ -13,13 +21,29 @@ export default {
     data() {
         return {
             comment: "Hello Driver",
-            fname: ''
+            fname: '',
+            name: 'Mombo Feisal'
+        }
+    },
+    methods: {
+        handleClick: function() {
+            alert('test')
+        },
+
+        changeName: function() {
+          this.name = 'Flavius'
+          console.log(this.name)
+        }
+    },
+    watch: {
+        name: function() {
+            console.log(this.name)
         }
     }
 
 }
 </script>
 
-<style lang="scss" scoped>
+<style scoped>
 
 </style>
